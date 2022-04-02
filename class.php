e893644cb8513b794bc8fa36e02eed048f126ddc
celebrater <tkgm45nbchk@gmail.com>
2022-04-03 04:33:31 +0900

    moved php_hp_bbs_2 and fp_common_modules into fujimipolis temporary

--- class.php
+++ class.php
@@ -21,13 +21,15 @@ class _main_ extends database
 
     public function GetData()
     {
+//        echo "Hello World from Getdata()";
         $query = $this->db->prepare("SELECT * FROM data_table LIMIT 1");
         $query->execute();
         $result = $query->fetch(PDO::FETCH_ASSOC);
 
         if (isset($result)) {
             $data = $result["name"];
-            die('{"result":"success", "name":"' . $data . '"}');
+//            die('{"result":"success", "name":"' . $data . '"}');
+            die('{"items": [{ "id": 1, "name": "Apples",  "price": "$2" }, { "id": 2, "name": "Peaches", "price": "$5" }]}');
         } else {
             die('{"result":"failed"}');
         }
--- index.php
+++ index.php
@@ -8,13 +8,15 @@ $main = new _main_;
 if (isset($_POST['type'])) $Type = $_POST['type'];
 if (isset($_POST['data'])) $Data = $_POST['data'];
 
-if (isset($Type)) {
-    switch (strip_tags($Type)) {
-        case "t_insert_data":
-            $main->InsertData($Data);
-            break;
-        case "t_get_data":
-            $main->GetData();
-            break;
-    }
-}
+//if (isset($Type)) {
+//    switch (strip_tags($Type)) {
+//        case "t_insert_data":
+//            $main->InsertData($Data);
+//            break;
+//        case "t_get_data":
+//            $main->GetData();
+//            break;
+//    }
+//}
+
+$main->GetData();
